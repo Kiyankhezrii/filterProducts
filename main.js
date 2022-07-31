b69b182a232cfc26a0d7f1a2f619c85e9434e431
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-07-31 17:37:21 +0430

    add render products function and handel DOMContentLoaded eventlisteners

--- main.js
+++ main.js
@@ -3,8 +3,31 @@ import { products } from "./products.js";
 // get element from DOM
 const input = document.querySelector(".form-input");
 const searchTitle = document.querySelector(".search-item");
+const productsContainer = document.querySelector(".container");
 
 const search = {
   filterSearch: "",
 };
 
+const renderProducts = function (product) {
+  const html = `
+        <div class="product">
+          <div class="img-container">
+            <img
+              src="${product.image}"
+              alt="products"
+              class="product__img"
+            />
+          </div>
+          <div class="product-info">
+            <p class="price">$ ${product.price}</p>
+            <p class="title">${product.title}</p>
+          </div>
+        </div>
+    `;
+    productsContainer.insertAdjacentHTML("afterbegin",html)
+};
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  products.forEach((p) => renderProducts(p));
+});
