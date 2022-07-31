990fccee176904969a6b5f2b38a3bad529edbf00
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-07-31 17:33:26 +0430

    get elements from DOM and import products

---
+++ main.js
@@ -0,0 +1,10 @@
+import { products } from "./products.js";
+
+// get element from DOM
+const input = document.querySelector(".form-input");
+const searchTitle = document.querySelector(".search-item");
+
+const search = {
+  filterSearch: "",
+};
+
